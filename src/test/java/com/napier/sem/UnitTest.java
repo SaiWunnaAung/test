e5e951fa77e472bb972517c190e15d896b052af3
SaiWunnaAung <40501792@live.napier.ac.uk>
2022-03-30 00:46:55 +0630

    Add unit test

---
+++ src/test/java/com/napier/sem/UnitTest.java
@@ -0,0 +1,34 @@
+package com.napier.sem;
+
+import com.napier.sem.model.Country;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+
+
+class UnitTest
+{
+    static Report report;
+
+    @BeforeAll
+    static void init()
+    {
+        report = new Report();
+    }
+
+    @Test
+    void printSalariesTestNull()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country country = new Country();
+        country.setCode("code");
+        country.setName("Name");
+        country.setContinent("continent");
+        country.setRegion("region");
+        country.setPopulation(2000);
+        country.setCapital("capital");
+        countries.add(country);
+        report.countryReportTemplate(countries);
+    }
+}
