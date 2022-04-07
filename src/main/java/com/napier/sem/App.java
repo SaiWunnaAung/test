6dd842fd001eae77643142e44659f71994b0f7d1
SaiWunnaAung <40501792@live.napier.ac.uk>
2022-04-08 00:11:20 +0630

    Last fix2

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -24,13 +24,13 @@ public class App
     public static void main(String[] args) throws ClassNotFoundException, InterruptedException, SQLException {
 //         Create  App object
         System.out.println("Hello");
-        app = new App();
-//        Add database connection
-        if (args.length < 1) {
-            db.connect("localhost:33060",0);
-        } else {
-            db.connect(args[0],Integer.parseInt(args[1]));
-        }
+//        app = new App();
+////        Add database connection
+//        if (args.length < 1) {
+//            db.connect("localhost:33060",0);
+//        } else {
+//            db.connect(args[0],Integer.parseInt(args[1]));
+//        }
 //        app.reportMenu();
         SpringApplication.run(App.class, args);
         //Remove database connection
